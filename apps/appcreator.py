48965e7b5b53513a770ea07c507e0fa376ab48b0
fxua <fxua@gmail.com>
2018-10-22 23:48:37 -0700

    recover athena.yaml

--- apps/appcreator.py
+++ apps/appcreator.py
@@ -6,13 +6,15 @@ Created on Sep 4, 2018
 from apps.spm.spamfilter import SpamFilter
 from apps.spm.spam_multifilters import SpamMultiFilters
 from apps.fts.forex_tick_sampler import ForexTickSampler
+from apps.generic_app.generic_app import GenericApp
 from apps.forex_trainer.fx_trainer_creator import createForexTrainer
 AppSwitcher = {
     99: SpamFilter.getInstance,
     98: SpamMultiFilters.getInstance,
     
     0:  ForexTickSampler.getInstance,
-    1:  createForexTrainer
+    1:  createForexTrainer,
+    2:  GenericApp.getInstance
     }
 
 def createApp(appType):
