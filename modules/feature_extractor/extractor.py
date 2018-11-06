f9f774be236902a8c47a9be42eff1341689fdea8
fxua <fxua@gmail.com>
2018-11-05 20:19:35 -0800

    base feature done

--- modules/feature_extractor/extractor.py
+++ modules/feature_extractor/extractor.py
@@ -10,6 +10,11 @@ import cPickle
 from modules.basics.common.logger import *
 import numpy as np
 from sklearn.preprocessing.data import StandardScaler
+from modules.basics.conf.mlengineconf import gMLEngineConfig
+from modules.basics.conf.generalconf import gGeneralConfig
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 
 class FeatureExtractor(object):
     def __init__(self):
@@ -19,7 +24,22 @@ class FeatureExtractor(object):
         self.testTargets = None
         self.verifyFeatureMatrix = None
         self.verifyTargets = None
-        self.scaler = StandardScaler()
+        self.scaler = None
+
+        if gGeneralConfig.isEnablePCA():
+            self.pca = self.createPCA()
+
+        if gMLEngineConfig.getEngineCoreType() >=2:
+            Log(LOG_INFO) << "Neural network needs no scaler"
+            return
+
+        sf = gGeneralConfig.getLoadScalerFile()
+        if sf == "":
+            self.createScaler()
+            Log(LOG_INFO) << "No scaler is given, created one"
+        else:
+            self.loadScaler(sf)
+            Log(LOG_INFO) << "Scaler loaded: %s" % sf
         return
     
     def save(self,filename,model):
@@ -33,6 +53,18 @@ class FeatureExtractor(object):
         model = cPickle.load(stream)
         stream.close()
         return model
+
+    def loadScaler(self,filename):
+        self.scaler = joblib.load(filename)
+        return
+
+    def createScaler(self):
+        self.scaler = StandardScaler()
+        return
+
+    def saveScaler(self,filename):
+        joblib.dump(self.scaler,filename)
+        return
     
     def getTrainFeatureMatrix(self):
         if self.trainFeatureMatrix is None:
@@ -67,8 +99,7 @@ class FeatureExtractor(object):
     def setTestTargets(self,targets):
         self.testTargets = targets
         return
-        
-    
+
     def prepare(self,args=None):
         return
     
@@ -79,4 +110,13 @@ class FeatureExtractor(object):
     def extractTestFeatures(self):
         Log(LOG_FATAL) << "Should be implemented in concrete extractors"
         return
-    
+
+    def createPCA(self):
+        pf = gGeneralConfig.getLoadPCAFile()
+        if pf == "":
+            pca = PCA(n_components=gGeneralConfig.getPCAComponents())
+            Log(LOG_INFO) << "PCA is enabled with %d components" % gGeneralConfig.getPCAComponents()
+        else:
+            Log(LOG_INFO) << "loading pca not supported"
+
+        return pca
