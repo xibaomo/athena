4eec52f6a5bb8ceea8aa94af72f5e355117ca986
fxua <fxua@gmail.com>
2019-05-26 17:03:41 -0700

    pair trader config

---
+++ api/api_athena/src/apiserver/server_apps/minbar_pair/mb_pairtrader.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mb_pairtrader.h
+ *
+ *    Description:
+ *
+ *
+ *        Version:  1.0
+ *        Created:  05/26/2019 04:39:06 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#ifndef  _SERVER_APP_MINBAR_PAIR_TRADER_H_
+#define  _SERVER_APP_MINBAR_PAIR_TRADER_H_
+
+#include "server_apps/server_base_app/server_base_app.h"
+
+class MinBarPairTrader : public Server
+#endif   /* ----- #ifndef _SERVER_APP_MINBAR_PAIR_TRADER_H_  ----- */
---
+++ api/api_athena/src/apiserver/server_apps/minbar_pair/mptconf.h
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mptconf.h
+ *
+ *    Description:  Configs for pair trader
+ *
+ *        Version:  1.0
+ *        Created:  05/26/2019 04:41:32 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#ifndef  _MINBAR_PAIR_TRADER_CONFIG_H_
+#define  _MINBAR_PAIR_TRADER_CONFIG_H_
+
+#include "basics/baseconf.h"
+const String MPT_ROOT = "MINBAR_PAIR_TRADER/";
+
+class MptConfig : public BaseConfig {
+private:
+    MptConfig() {;}
+public:
+    virtual ~MptConfig() {;}
+
+    static MptConfig& getInstance() {
+        static MptConfig _ins;
+        return _ins;
+    }
+
+    String getPairASym() {
+        return getKeyValue<String>(MPT_ROOT + "PAIR_A_SYM");
+    }
+
+    String getPairASym() {
+        return getKeyValue<String>(MPT_ROOT + "PAIR_B_SYM");
+    }
+
+    int getLRLen() {
+        return getKeyValue<int>(MPT_ROOT + "LR_LEN");
+    }
+
+    real32 getThresholdStd() {
+        return getKeyValue<real32>(MPT_ROOT + "THRESHOLD_STD");
+    }
+};
+#endif   /* ----- #ifndef _MINBAR_PAIR_TRADER_CONFIG_H_  ----- */
