1f248eca4f29122b22a36546032e078952300207
fxua <fxua@gmail.com>
2023-07-03 02:58:05 -0700

    use ma of rtn and std close to the target date

--- py_algos/portfolio/min_std_max_rtn.py
+++ py_algos/portfolio/min_std_max_rtn.py
@@ -39,7 +39,7 @@ def rtn_cost(wts,sym_rtns):
     for w,v in zip(ws,sym_rtns):
         s += w*v
     return s
-def std_cost(wts,daily_rtns):
+def __std_cost(wts,daily_rtns):
     if np.sum(wts) >= 1.:
         return 100
     ws = copy.deepcopy(wts)
@@ -65,6 +65,36 @@ def std_cost(wts,daily_rtns):
                 sj = sds[j]
                 s += wi*wj*cm[i,j]*si*sj
     return np.sqrt(s)
+def std_cost(wts,cm,sym_std):
+    if np.sum(wts) >= 1.:
+        return 100
+    ws = copy.deepcopy(wts)
+    ws = np.append(ws,1-np.sum(ws))
+    if np.any(ws < 0):
+        return 100
+    s = 0.
+    sds = sym_std
+    for i in range(len(ws)):
+        for j in range(len(ws)):
+            if i == j:
+                w = ws[i]
+                sd = sds[i]
+                s += w**2 * sd**2
+            else:
+                wi = ws[i]
+                wj = ws[j]
+                si = sds[i]
+                sj = sds[j]
+                s += wi*wj*cm[i,j]*si*sj
+    return np.sqrt(s)
+
+def snap_target_date(date_str,df):
+    # pdb.set_trace()
+    tar_date = pd.to_datetime(date_str)
+    for i in range(len(df)):
+        dt = tar_date - df.index[i]
+        if dt.days < 3:
+            return i
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -84,18 +114,23 @@ if __name__ == "__main__":
 
     data = yf.download(syms.tolist(), start = start_date, end = end_date)['Adj Close']
     daily_rtns = data.pct_change()
+    # pdb.set_trace()
     ### estimate expectation of return of each symbol
     monthly_avg_return = daily_rtns.resample('M').mean()
-    # Compute 5-month moving average of monthly average daily return
     monthly_avg_return_ma = monthly_avg_return.rolling(window=portconf.getMAWindow()).mean()
-    sym_rtns = monthly_avg_return_ma.iloc[-1,:]
+    # sym_rtns = monthly_avg_return_ma.iloc[-1,:]
+    tid = snap_target_date(portconf.getTargetDate(),monthly_avg_return_ma)
+    sym_rtns = monthly_avg_return_ma.iloc[tid,:].values
+    # pdb.set_trace()
+    ### estimate std of return of each symbol
+    monthly_avg_std = daily_rtns.resample('M').std()
+    cm = daily_rtns.corr().values
+    monthly_avg_std_ma = monthly_avg_std.rolling(window=portconf.getMAWindow()).mean()
+    sym_std = monthly_avg_std_ma.iloc[tid, :].values
     def obj_func(ws,dayrtn):
         t1 = rtn_cost(ws,sym_rtns)
-        t2 = std_cost(ws,dayrtn)
+        t2 = std_cost(ws,cm,sym_std)
         return t2-t1
-    # wts = np.ones(NUM_SYMS-1)/float(NUM_SYMS)
-    # t1 = rtn_cost(wts,daily_rtns)
-    # t2 = std_cost(wts,daily_rtns)
 
     sol,_ = ga_minimize(obj_func,daily_rtns,len(syms)-1,num_generations=gaconf.getNumGenerations(),population_size=gaconf.getPopulation(),
                         cross_prob=gaconf.getCrossProb(),mutation_rate=gaconf.getMutateProb())
@@ -103,4 +138,4 @@ if __name__ == "__main__":
     print("selected syms: ",picked_rows['<SYM>'].values)
     print("best solution: ",sol,1-np.sum(sol))
     print("best rtn: {}".format(rtn_cost(sol,sym_rtns)))
-    print("best std: {}".format(std_cost(sol, daily_rtns)))
+    print("best std: {}".format(std_cost(sol, cm,sym_std)))
