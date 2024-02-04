505cf059598d994462debfc2ec92c7115d640d88
fxua <fxua@gmail.com>
2024-02-04 07:01:38 -0800

    mu_info does not give better profit. it is often that low correlation has high mutual info. we favor linear dependency

--- py_algos/stock_portfolio/sym_selection.py
+++ py_algos/stock_portfolio/sym_selection.py
@@ -1,16 +1,39 @@
 import pdb
-
 from mkv_absorb import *
 import numpy as np
-import pdb
+from sklearn.feature_selection import mutual_info_regression
+import matplotlib.pyplot as plt
+def __transmat2dist(transmat):
+    nsyms = transmat.shape[0]
+    I = np.eye(nsyms)
+    ONE = np.ones((nsyms, nsyms))
+    row_one = np.ones(nsyms)
+    tmp = I - transmat + ONE
+    tmp = np.linalg.inv(tmp)
+    sol = np.matmul(row_one, tmp)
+    return sol
+def transmat2dist(transmat):
+    nsyms = transmat.shape[0]
+    x = np.ones([nsyms,1])
+    # pdb.set_trace()
+    for i in range(nsyms):
+        if transmat[i,i]==1:
+            x[i,0] = 0
 
-def corr2distScore(cm):
+    t_trans = transmat.transpose()
+    for i in range(100):
+        x = np.matmul(t_trans,x)
+    return x.flatten()
+def corr2distScore(cm,df):
     nsyms = cm.shape[0]
     # pdb.set_trace()
     transmat = np.zeros((nsyms,nsyms))
     for i in range(nsyms):
         for j in range(i+1,nsyms):
-            if cm[i,j] < 0:
+            if cm[i,j] <= -.05:
+                x = df.iloc[:,i].values.reshape([-1,1])
+                y = df.iloc[:,j].values
+                # transmat[i,j] = mutual_info_regression(x,y)
                 transmat[i,j] = -cm[i,j]
                 transmat[j,i] = transmat[i,j]
 
@@ -18,23 +41,40 @@ def corr2distScore(cm):
     for i in range(nsyms):
         s = sum(transmat[i,:])
         if s==0:
-            print("===================== loner found ==================")
-        transmat[i,:] = transmat[i,:]/s
+            # print("===================== loner found ==================")
+            transmat[i,i] = 1
+        else:
+            transmat[i,:] = transmat[i,:]/s
 
     # pdb.set_trace()
-    I = np.eye(nsyms)
-    ONE = np.ones((nsyms,nsyms))
-    row_one = np.ones(nsyms)
-    tmp = I - transmat + ONE
-    tmp = np.linalg.inv(tmp)
-    sol = np.matmul(row_one,tmp)
-    return sol
+    score = transmat2dist(transmat)
+    return score
+def check_mutual_info(df,cm):
+    c=[]
+    s=[]
+    for i in range(cm.shape[0]):
+        for j in range(i+1,cm.shape[1]):
+            if cm[i,j] < 0:
+                x = df.iloc[:,i].values.reshape([-1,1])
+                y = df.iloc[:,j].values
+                score = mutual_info_regression(x,y)[0]
+                c.append(cm[i,j])
+                s.append(score)
+                print("corr: {}, mutual_score: {}".format(cm[i,j],score))
+                if cm[i,j] > -0.1 and score > 1.0:
+                    plt.plot(x,y,'.')
+                    plt.show()
+
+    plt.plot(c,s,'.')
+    plt.show()
 
 def select_syms_corr_dist(df,num_syms):
     cm = df.corr().values
-    s1 = corr2distScore(cm)
-    cm2 = df.iloc[-30:,:].corr().values
-    s2 = corr2distScore(cm2)
+    # check_mutual_info(df,cm)
+    s1 = corr2distScore(cm,df)
+    df2 = df.iloc[-30:,:]
+    cm2 = df2.corr().values
+    s2 = corr2distScore(cm2,df2)
     score = np.array(s1+s2)
     # pdb.set_trace()
     sorted_id = np.argsort(score)[::-1]
