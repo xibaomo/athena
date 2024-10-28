b93182129ee6e862f6ec212fc13c851fb7ead71b
fxua <fxua@gmail.com>
2024-10-27 19:53:13 -0700

    add minimizer but not work well

--- py_algos/pair_options/rboption.py
+++ py_algos/pair_options/rboption.py
@@ -3,6 +3,7 @@ import pickle,sys,os
 import time
 import random
 from datetime import datetime,timedelta
+from scipy.optimize import minimize
 
 import robin_stocks.robinhood as rh
 from utils import *
@@ -81,7 +82,7 @@ def computeBestPair(ub_rtn, lb_rtn, cur_price, df, call_opts,put_opts):
     rtns = df['Close'].pct_change().values[1:]
     mi = -1
     mj = -1
-    max_prof = -999999
+    max_daily_rtn = -999999
     probcal = FirstHitProbCal(rtns,500,ub_rtn,lb_rtn)
     # cur_price = float(rh.stocks.get_latest_price(ticker)[0])
     print("Latest price: ", cur_price)
@@ -95,42 +96,48 @@ def computeBestPair(ub_rtn, lb_rtn, cur_price, df, call_opts,put_opts):
         call_days = callopt['days']
         for j in range(len(put_opts)):
             putopt = put_opts[j]
+            putcost = float(putopt['ask_price'])
             put_days = putopt['days']
             expire_date = callopt['expiration_date'] if call_days < put_days else putopt['expiration_date']
             pu,pd = probcal.get1stHitProbs(expire_date)
-            tmp_up = call_prof - float(putopt['ask_price'])
-            tmp_dw = float(putopt['strike_price']) - cur_price*(1+lb_rtn) - float(putopt['ask_price']) - callcost
-
-            expect_prof = tmp_up * pu + tmp_dw * pd
-            if expect_prof > max_prof and pu+pd>=.7 and tmp_up*tmp_dw>0:
-                max_prof = expect_prof
+            tmp_up = call_prof - putcost
+            tmp_dw = float(putopt['strike_price']) - cur_price*(1+lb_rtn) - putcost - callcost
+            trade_days = call_days if call_days<put_days else put_days
+            cost = float(putopt['ask_price']) + callcost
+            expect_prof = tmp_up * .5 + tmp_dw * .5
+            daily_rtn = expect_prof/trade_days/cost
+            if daily_rtn > max_daily_rtn and pu+pd>=.7 and tmp_up*tmp_dw>0 :
+                max_daily_rtn = daily_rtn
                 mi = i
                 mj = j
                 # print("max expected profit: ", max_prof)
                 call_profit = tmp_up
                 put_profit = tmp_dw
 
-    print(f"\033[91moptimal expected profit: {max_prof:.2f}, {call_profit:.2f}, {put_profit:.2f}\033[0m")
+    print(f"\033[91moptimal daily rtn: {max_daily_rtn:.3f}\033[0m")
+    print(f"\033[91mcall,put profit: {call_profit:.2f}, {put_profit:.2f}\033[0m")
+
     # pdb.set_trace()
     days1 = call_opts[mi]['days']
     days2 = put_opts[mj]['days']
     expire_date = call_opts[mi]['expiration_date'] if days1 < days2 else put_opts[mj]['expiration_date']
-    print("trade days: ", days1 if days1 < days2 else days2)
+
     pu,pd = probcal.get1stHitProbs(expire_date)
     print(f"up,down probs: {pu:.3f}, {pd:.3f}, {pu+pd:.3f}")
+    print("trade days: ", days1 if days1<days2 else days2)
 
     print(call_opts[mi])
     print(put_opts[mj])
-    return mi,mj
+    return max_daily_rtn, mi,mj
 
 # Example usage: Get option chain for a stock ticker (e.g., 'AAPL')
 # Log into Robinhood using the credentials
 
 data_file = ticker + "_" + data_file
 
-# options=get_option_chain(ticker)
-# with open(data_file, 'wb') as f:
-#     pickle.dump(options,f)
+options=get_option_chain(ticker)
+with open(data_file, 'wb') as f:
+    pickle.dump(options,f)
 
 with open(data_file,'rb') as f:
     options = pickle.load(f)
@@ -141,6 +148,8 @@ days_counter = TradeDaysCounter()
 for opt in options:
     days = days_counter.countTradeDays(opt['expiration_date'])
     opt['days'] = days
+    if days >= 60:
+        continue
     if opt['ask_price'] < 0:
         continue
     if opt['type'] == 'call':
@@ -154,10 +163,28 @@ call_profit=0
 put_profit=0
 
 df = download_from_robinhood(ticker)
-r = 0.02
-for i in range(8):
-    rb = 0.02*i +0.02
-    print("r = ",rb)
-    mi,mj = computeBestPair(rb, -rb, cur_price, df, call_opts,put_opts)
+def obj_func(x,params):
+    cur_price,df,call_opts,put_ops = params
+    rb = x[0]
+    # pdb.set_trace()
+    mdp,mi,mj = computeBestPair(rb,-rb,cur_price,df,call_opts,put_opts)
+    print(f"rtn bound: {rb}, max daily return: {mdp}")
+    return -mdp
+# bounds=[(0.05,0.15)]
+# x0=np.array([.1])
+# result = minimize(obj_func,x0,args=((cur_price,df,call_opts,put_opts),),bounds=bounds,method='COBYLA',tol=1e-3)
+# xo = result.x[0]
+# print("Best return: ", xo)
+# mdp,mi,mj = computeBestPair(xo,-xo,cur_price,df,call_opts,put_opts)
 
+dps = []
+rbs = [x*0.01+0.03 for x in range(15)]
+for rb in rbs:
+    print("r = ",rb)
+    mdp, mi,mj = computeBestPair(rb, -rb, cur_price, df, call_opts,put_opts)
+    dps.append(mdp)
+
+import matplotlib.pyplot as plt
+plt.plot(rbs,dps,'.')
+plt.show()
 # Log out after done
