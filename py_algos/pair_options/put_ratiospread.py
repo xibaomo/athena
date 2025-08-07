5f8a3250cdf4ac202e5ae0a236e81770c049ccfe
fxua <fxua@gmail.com>
2025-08-07 05:06:37 -0700

    put ratio spread is up

---
+++ py_algos/pair_options/put_ratiospread.py
@@ -0,0 +1,129 @@
+import os,sys
+import pdb
+from datetime import datetime,timedelta
+
+import numpy as np
+import yfinance as yf
+from scipy import stats
+from cal_prob import findBestLookbackDays,prepare_rtns
+from utils import *
+from mkv_cal import *
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from option_chain import *
+
+
+def compExpectedReturn(cur_price, s_hi,s_lw, premium, probs, drtn,lb_rtn,ratio):
+    expected_rtn = 0.
+    max_loss = s_lw*ratio - s_hi #max_loss is the cash secured (margin)
+
+    for i in range(len(probs)):
+        r = lb_rtn + (i+0.5)*drtn
+        p = cur_price*(1+r)
+        if p >= s_hi:
+            rtn = premium/max_loss
+        elif p < s_hi and p > s_lw:
+            rtn = (s_hi - p + premium)/max_loss
+        else:
+            rev = s_hi - p
+            loss = (s_lw-p)*ratio
+            rtn = (rev - loss + premium)/max_loss
+            # rtn=0
+        expected_rtn = expected_rtn + probs[i]*rtn
+    return expected_rtn
+
+def find_match_put(opt_idx, options, ratio):
+    market_value = float(options[opt_idx]['bid'])*ratio
+    for i in range(opt_idx,len(options)):
+        ask = float(options[i]['ask'])
+        if ask >= market_value:
+            return i-1
+    # not found
+    return -2
+
+
+def prepare_puts(sym,exp_date):
+    options = get_option_chain_alpha_vantage(sym)
+    print(f"{len(options)} options downloaded")
+    puts = []
+    for opt in options:
+        if opt['expiration'] == exp_date and opt['type'] == 'put':
+            puts.append(opt)
+
+    print(f"{len(puts)} puts returned")
+    puts = sorted(puts, key=lambda x: float(x["strike"]))
+    return puts
+
+def calibrate_strike_put(cur_price, puts, rtns, steps, lb_rtn , ub_rtn,ratio):
+    max_rtn = -999990.0
+    best_strike = []
+    best_premium = 0
+
+    probs = compMultiStepProb(rtns,steps,lb_rtn,ub_rtn)
+
+    x = np.linspace(lb_rtn,ub_rtn,len(probs))
+    plt.plot(x,probs,'.')
+    # plt.show()
+    # pdb.set_trace()
+    drtn = (ub_rtn - lb_rtn) / len(probs)
+    for i in range(len(puts)):
+        put = puts[i]
+        if float(put['bid']) < 1.:
+            continue
+        strike = float(put['strike'])
+        match_idx = find_match_put(i,puts,ratio)
+        if match_idx < 0:
+            continue
+        strike_rtn = strike/cur_price - 1.
+        if strike_rtn >= ub_rtn or strike_rtn <= lb_rtn:
+            continue
+        premium = float(put['bid'])*ratio - float(puts[match_idx]['ask'])
+        s_lw = strike
+        s_hi = float(puts[match_idx]['strike'])
+        exp_rtn = compExpectedReturn(cur_price,s_hi=s_hi,s_lw=s_lw,premium=premium,probs=probs,drtn=drtn,lb_rtn=lb_rtn,ratio=ratio)
+        # pdb.set_trace()
+
+        print(f"strike pair: {strike},{s_hi}, exp_rtn: {exp_rtn:.4f}, premium: {put['bid']},{puts[match_idx]['ask']}, {premium:.2f}")
+        if exp_rtn > max_rtn:
+            max_rtn = exp_rtn
+            best_strike = [s_lw,s_hi]
+            best_premium = premium
+
+    safe_price = (-best_premium - best_strike[1] + ratio*best_strike[0])/(ratio-1)
+    return best_strike, max_rtn, safe_price
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} <ticker> <expiration_date>")
+        sys.exit(0)
+
+    ticker = sys.argv[1]
+    exp_date = sys.argv[2]
+
+    fwd_days = TradeDaysCounter().countTradeDays(exp_date)
+    print(f"trading days: {fwd_days}")
+    df, bars_per_day = download_from_yfinance(ticker, period='2y')
+
+    # rtns = df['Open'].pct_change().values
+    rtns, bars_per_day = prepare_rtns(df, bars_per_day)
+    cur_price = df['Close'].values[-1][0]
+
+
+
+    # spacing,min_diff = find_stablest_spacing(rtns,22*bars_per_day,2*bars_per_day)
+    # print(f"length of rtns: {len(rtns)}, min ave diff: {min_diff}, spacing days: {spacing//bars_per_day}")
+
+    lookback_days, min_diff = findBestLookbackDays(22 *6 , 22 * 22, fwd_days, bars_per_day, rtns)
+    print(f"optimal days: {lookback_days}, min_diff: {min_diff}")
+
+    pick_rtns = rtns[-lookback_days*bars_per_day:]
+
+    puts = prepare_puts(ticker,exp_date)
+    # pdb.set_trace()
+
+    steps = fwd_days*bars_per_day
+    best_strike,max_rtn,safe_price = calibrate_strike_put(cur_price,puts,pick_rtns,steps,lb_rtn = -0.5, ub_rtn = 1., ratio=3)
+    print(f"Latest price: {cur_price:.2f}")
+    print(f"best strike: {best_strike}, max_rtn: {max_rtn}, safe_price: {safe_price:.2f}")
+    print(f"max daily return: {max_rtn/fwd_days:.4f}, annual return: {max_rtn/fwd_days*252:.4f}")
+    plt.show()
